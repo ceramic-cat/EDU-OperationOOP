ba1f6eb4cceb5805810373d48da9fa773a502d15
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-02-26 11:22:35 +0100

    added keyboard models

---
+++ OperationOOP.Core/Models/Keyboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationOOP.Core.Models;
+public class Keyboard : PurchasableProduct
+{
+    public bool HasRGB { get; set; } = false;
+    public bool HasBluetooth { get; set; } = false;
+    public (string Designation, int AmountOfKeys) FormFactor {  get; set; }
+    public KeyCap KeyCapType { get; set; }
+    public KeySwitch KeySwitch {  get; set; }
+
+}
